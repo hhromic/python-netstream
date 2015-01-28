37c37510e1584f19cbe1e06fa00d16c59b043c91
Hugo Hromic <hugo.hromic@deri.org>
2015-01-28 00:50:07 +0000

    Initial skeleton

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = "netstream",
+  packages = ["netstream"],
+  version = "0.1",
+  description = "A NetStream implementation in Python for GraphStream",
+  author = "Hugo Hromic",
+  author_email = "hhromic@gmail.com",
+  url = "https://github.com/hhromic/python-netstream",
+  download_url = "https://github.com/hhromic/python-netstream/tarball/0.1",
+  keywords = ["netstream", "graphstream"],
+  classifiers = [],
+)
